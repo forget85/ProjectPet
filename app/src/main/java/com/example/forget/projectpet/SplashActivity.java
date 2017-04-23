cdb283d82efb2c9f8490fdda12eb4ee1d9235812
forget <forget_@naver.com>
2017-04-23 18:15:22 +0900

    Splash Activity와 MainActivity 추가

--- app/src/main/java/com/example/forget/projectpet/SplashActivity.java
+++ app/src/main/java/com/example/forget/projectpet/SplashActivity.java
@@ -1,13 +1,23 @@
 package com.example.forget.projectpet;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        };
+        handler.postDelayed(runnable, 2000);
     }
 }
